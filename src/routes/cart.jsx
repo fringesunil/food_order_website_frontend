0e73192cf107133a5899c5d705c6ad2a4fe8c2b0
Fringe sunil <fringeks011@gmail.com>
2024-09-18 21:34:20 +0530

    Add cart and profile page

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link, Outlet } from "react-router-dom";
 import CartitemsCard from '../components/CartitemsCard'
 
 export default function Cart() {
@@ -12,7 +13,39 @@ export default function Cart() {
           <CartitemsCard/>
         </div>
         <div className='w-[59rem] h-screen bg-transparent border-r border-black px-3 py-1'>
-        
+          <h2>Coupon Code</h2>
+          <input type="couponcode" id="couponcode" className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"/>
+          <div className="flex items-center justify-between py-2">
+          <button className="w-full bg-[#A5B5BF] text-black font-bold py-2 px-4 rounded-full  border-2 border-black" type="button">
+           <Link to={`/home/hotels`}>
+           Apply
+           </Link>
+            </button>
+          </div>
+          <h2>Bill Details</h2>
+          <div className='flex justify-between'>
+          <span>Item Total</span>
+          <span>₹ 80</span>
+          </div>
+          <div className='flex justify-between'>
+          <span>Discount</span>
+          <span>₹ 0</span>
+          </div>
+          <div className='flex justify-between'>
+          <span>Gst</span>
+          <span>₹ 14</span>
+          </div>
+          <div className='flex justify-between font-bold'>
+          <span>To Pay</span>
+          <span>₹ 94</span>
+          </div>
+          <div className="flex items-center justify-between py-2">
+          <button className="w-full bg-[#A5B5BF] text-black font-bold py-2 px-4 rounded-full  border-2 border-black" type="button">
+           <Link to={`/home/hotels`}>
+           Proceed To Pay
+           </Link>
+            </button>
+          </div>
         </div>
       </div>
     </section>
--- src/routes/profile.jsx
+++ src/routes/profile.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   return (
     <main className='bg-[#B0A1BA] h-screen'>
     <section >
-        <h1>Profile</h1>
+        <div className='flex justify-center items-center flex-col'>
+        <img src="https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?auto=compress&cs=tinysrgb&w=600" alt="profile picture" className='w-[10rem] h-[10rem] my-1 mx-6 rounded-full mb-4' />
+        <h1 className='mb-4 font-bold'>Fringe Sunil</h1>
+        <button className="w-[25rem] bg-[#A5B5BF] text-black font-bold py-2 px-4 rounded  border-2 border-black mb-4" type="button">
+           <Link to={`/home/hotels`}>
+           Manage Address
+           </Link>
+            </button>
+            <button className="w-[25rem] bg-[#A5B5BF] text-black font-bold py-2 px-4 rounded  border-2 border-black mb-4" type="button">
+           <Link to={`/`}>
+           Logout
+           </Link>
+            </button>
+        </div>
        
     </section>
   </main>
